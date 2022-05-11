6cbc7ce5f40700b835008044c9756631d3e9b0d7
Storm <storm.sarracino01@gmail.com>
2022-05-11 15:58:19 -0500

    some controller function

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,11 @@
 class DrinksController < ApplicationController
+   def index
+      drinks = Drink.all 
+      render json: drinks
+   end
+
+   def show
+      drink = Drink.find(params[:id])
+      render json: drink
+   end
 end
