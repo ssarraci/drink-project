ddd3f79e77fcb6bbf145e07406dc0701454b71d1
Storm <storm.sarracino01@gmail.com>
2022-05-22 04:06:19 -0500

    Controller actions for user

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -6,6 +6,21 @@ class DrinksController < ApplicationController
 
    def show
       drink = Drink.find(params[:id])
-      render json: drink
+      if drink
+         render json: drink
+      else
+         render json: { error: "Drink not found" }, status: :not_found
+      end
+   end
+
+   def create
+      drink = Drink.create(drink_params)
+      render json: drink, status: :created
+   end
+
+   private
+
+   def drink_params
+      params.permit(:name, :ingredients, :alcoholic)
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :create, :update]
 
-  resources :orders, only: [:index, :show]
+  resources :orders, only: [:index, :show, :create]
 
-  resources :drinks, only: [:index, :show]
+  resources :drinks, only: [:index, :show, :create]
 end
